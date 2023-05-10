de392d8c7c4f07eec60e9ff4fc70332cd87b1637
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-05-10 17:35:37 +0100

    Task added

--- src/main/java/ua/finedefinition/jira/exception/ConfigSavingException.java
+++
@@ -1,7 +0,0 @@
-package ua.finedefinition.jira.exception;
-
-public class ConfigSavingException extends Exception {
-    public ConfigSavingException(String message) {
-        super(message);
-    }
-}
